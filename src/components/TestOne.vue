09ee6af229e5d82b85efaecf3fe0ee1813f0abfc
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-02 16:22:12 +0300

    Changed file contents in TestOne

---
+++ src/components/TestOne.vue
@@ -0,0 +1,6 @@
+import {mount} from "@vue/test-utils"; const FeedbackComponent = { props:['msg'],
+template: `
+<p>{{msg}}</p>
+` } test('displays message', () =>{ const wrapper = mount(FeedbackComponent,{ propsData:{
+msg: 'VueTesting implemetation'}}) expect(wrapper.text()).toContain('VueTesting
+implemetation');})
---
+++ src/tests/DisplayTest.vue
@@ -0,0 +1,43 @@
+// // import mount from vue dev utils 
+
+// import { mount } from '@vue/test-utils';
+
+// // define component
+// const FeedbackComponent = {
+    
+// }
+<template>
+    <div class="container">
+      {{ msg }}
+  
+      
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HelloWorld",
+    props: {
+      msg: String,
+    },
+  };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
