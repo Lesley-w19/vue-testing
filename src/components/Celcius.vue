ffea640333edcdad80da36af4f0c87692850e8fb
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-02 22:15:35 +0300

    Added the Celcius component: -- it  displays a message from data object -- defines an array -- converts fahrenheit to degrees of an array parameter

---
+++ src/components/Celcius.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h5>{{ message }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FahrenheitCelsius",
+  data() {
+    return {
+      message: "Fahrenheit to Celcius",
+      fah: [23, 140, 212, 41],
+    };
+  },
+  methods: {
+    getCelcius(fah) {
+      return fah.map((temp) => (temp - 32) * 5/9);
+    },
+  },
+};
+</script>
